61c6ec104ad8549bae6518660b816cf21009029d
boyko-marina <boyko.marina.a@mail.ru>
2026-02-17 16:51:54 +0300

    первый коммит

---
+++ get-dac/signal_generator_tri.py
@@ -0,0 +1,14 @@
+import numpy as np
+import time
+
+
+def get_sin_wave_amplitude(freq, time):
+    raw = np.sin(2.0 * np.pi * freq * time)
+    shifted = raw + 1
+    normalised = shifted / 2.0
+
+    return normalised
+
+def wait_for_sampling_period(sampling_frequency):
+    period = 1.0 / sampling_frequency
+    time.sleep(period)
